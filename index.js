0b9c740256438f9202c1d71282d068b7c137f213
egirlcatnip <egirlcatnip@gmail.com>
2023-10-21 16:38:51 +0200

    Create index.js
    Yayyy! Theme logic!!

---
+++ index.js
@@ -0,0 +1,27 @@
+/* Acrylic League of legends client theme */
+/* Adjusted by egirlcatnip from Sarah's acrylic theme */
+
+/* Version: 1.0 */
+
+/* credits: @aimslut(discord), @egirlcatnip(discord) */
+
+/* this source: https://github.com/PrincessAkira/league-launcher-theme/tree/main/Acrylical    */
+/* Sarah's source: https://github.com/PrincessAkira/league-launcher-theme/tree/main/Acrylical */
+
+/*
+Install guide:
+Create a folder (eg. "theme") inside the Pengu loader's plugin folder
+Add the .js and .css file inside.
+Your theme should be succesfully installed now! :3
+*/
+
+/*
+Disclaimer: this theme is *local*, meaning it will not receive updates automatically.
+You have to update this theme yourself, manually downloading a new release.
+*/
+
+/* Import the local theme file */
+import "./theme.css";
+
+/* Darken the transparency */
+window.Effect.apply('unified', { color: '#000000DA' });
