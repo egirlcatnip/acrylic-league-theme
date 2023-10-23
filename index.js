32c0685c98c13c286c01d9ac9e0ac303f1c6ab09
egirlcatnip <egirlcatnip@gmail.com>
2023-10-23 23:33:39 +0200

    Themed  lobby info panel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Acrylic League of legends client theme */
 /* Adjusted by egirlcatnip from Sarah's acrylic theme */
 
-/* Version: 1.1 */
+/* Version: 1.2 */
 
 /* credits: @aimslut(discord), @egirlcatnip(discord) */
 
@@ -20,8 +20,58 @@ Disclaimer: this theme is *local*, meaning it will not receive updates automatic
 You have to update this theme yourself, manually downloading a new release.
 */
 
+
+/* javascript to do stuffs: */
+
+/* apply the acrylic effect */
+window.Effect.apply('unified', { color: '#000000DA' });
+
+
 /* Import the local theme file */
 import "./theme.css";
 
-/* Darken the transparency */
-window.Effect.apply('unified', { color: '#000000DA' });
+/* Import shadow-dom obeserver-fixer-thing */
+import * as observer from './observer'
+
+
+/* Code to edit the lobby info panel, invites, leftmost panel, top of friend list */
+observer.subscribeToElementCreation('lol-parties-game-info-panel', (element) => {
+    const panelRoot = element.shadowRoot;
+    const statusCardRoot = panelRoot.querySelector("lol-parties-status-card").shadowRoot;
+    const gameInviteRoot = panelRoot.querySelector("lol-parties-game-invites").shadowRoot;
+
+    const statusCardStyle = document.createElement("style");
+    statusCardStyle.textContent = `
+        .parties-status-card {
+            background: none !important;
+            cursor: default !important;
+        }
+        .parties-status-card-bg-container > video {
+            display: none !important;
+        }
+        .parties-sttus-card-body
+
+        // .parties-status-card-header-text {
+        //     display: none !important;
+        // }
+        // .parties-status-card-header-icon {
+        //     display: none !important;
+        // }
+        parties-game-search-divider {
+            display: none !important;
+        }
+        // .parties-status-card-body {
+        //     padding: 10px 1px 10px 10px !important;
+        // }
+    `;
+
+    statusCardRoot.appendChild(statusCardStyle);
+
+    const gameInviteStyle = document.createElement("style");
+    gameInviteStyle.textContent = `
+        .parties-game-invite-heading-text{
+            display: none;
+        }
+    `;
+    gameInviteRoot.appendChild(gameInviteStyle);
+})
---
+++ observer.js
@@ -0,0 +1,85 @@
+/* Acrylic League of legends client theme */
+/* Adjusted by egirlcatnip from Sarah's acrylic theme */
+
+/* Version: 1.2 */
+
+/* credits: @aimslut(discord), @egirlcatnip(discord), @unproductive(discord) */
+
+/* this source: https://github.com/PrincessAkira/league-launcher-theme/tree/main/Acrylical    */
+/* Sarah's source: https://github.com/PrincessAkira/league-launcher-theme/tree/main/Acrylical */
+
+/* Code for the shadow-dom obeserver-fixer-thing */
+
+let tagCreationCallbacks = []
+let tagDeletionCallbacks = []
+let classCreationCallbacks = []
+let classDeletionCallbacks = []
+
+export function subscribeToElementCreation(target, callback) {
+    if (target[0] === '.') {
+        target = target.slice(1)
+        classCreationCallbacks.push({ target, callback })
+    } else {
+        tagCreationCallbacks.push({ target, callback })
+    }
+}
+
+export function subscribeToElementDeletion(target, callback) {
+    if (target[0] === '.') {
+        target = target.slice(1)
+        classDeletionCallbacks.push({ target, callback })
+    } else {
+        tagDeletionCallbacks.push({ target, callback })
+    }
+}
+
+const observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+        for (let node of mutation.addedNodes) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                let tagLowered = node.tagName.toLowerCase()
+                for (let obj of tagCreationCallbacks) {
+                    if (tagLowered.indexOf(obj.target) != -1) {
+                        obj.callback(node)
+                    }
+                }
+
+                var classList = node.classList
+                if (classList) {
+                    for (let nodeClass of classList) {
+                        let classLowered = nodeClass.toLowerCase()
+                        for (let obj of classCreationCallbacks) {
+                            if (classLowered.indexOf(obj.target) != -1) {
+                                obj.callback(node)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (let node of mutation.removedNodes) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                let tagLowered = node.tagName.toLowerCase()
+                for (let obj of tagDeletionCallbacks) {
+                    if (tagLowered.indexOf(obj.target) != -1) {
+                        obj.callback(node)
+                    }
+                }
+
+                var classList = node.classList
+                if (classList) {
+                    for (let nodeClass of classList) {
+                        let classLowered = nodeClass.toLowerCase()
+                        for (let obj of classDeletionCallbacks) {
+                            if (classLowered.indexOf(obj.target) != -1) {
+                                obj.callback(node)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+observer.observe(document, { attributes: false, childList: true, subtree: true })
